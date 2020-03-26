2ca6b2eb44c762c2477fb6b48ba5897572fdd21e
BautistaCRP <bj.carpintero@gmail.com>
2020-03-26 12:40:37 -0300

    TODO added with commands to deploy in github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,13 @@ export class AppComponent {
   private guiHandlerService: GuiHandlerService;
 
 
+  /*
+    TODO: To deploy in Github Pages use this commands:
+    ng build --prod --base-href "https://bautistacrp.github.io/ILP-Planning/"
+    * Copy the 404.html file into de dist/ILP-Planning folder
+    ngh --dir=dist/ILP-Planning
+    * Then execute ng serve to update the page -> https://bautistacrp.github.io/ILP-Planning/ 
+  */
 
   constructor(guiHandlerService: GuiHandlerService){
       this.guiHandlerService = guiHandlerService;
